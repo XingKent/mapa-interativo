27347547951fa72ddc8afc43f9b90403ddb4b552
XingKent <guifreire448@gmail.com>
2025-04-16 21:20:51 -0300

    mudando comentario

--- indicadores/urls.py
+++ indicadores/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # <- ESSA LINHA é importante
+    path('api/', include('api.urls')),  # <-ESSA LINHA é importante pfv nao mexer SERIO
 ]
 
