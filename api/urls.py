4ec1829ee6f0fd2a80f788311fe50e2d3319fd7b
XingKent <guifreire448@gmail.com>
2025-04-16 21:19:42 -0300

    link do site

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.homepage, name='homepage'),
     path('indicadores/<str:uf>/', views.indicadores_estado, name='indicadores_estado'),
 ]
