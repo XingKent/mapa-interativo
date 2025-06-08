9243ef3015c09e5373dda9bf9b35b6c98c216603
XingKent <guifreire448@gmail.com>
2025-06-08 03:24:07 -0300

    nova janela pra rodar o servidor

--- start_site.bat
+++ start_site.bat
@@ -1,14 +1,24 @@
 @echo off
-echo Criando ambiente virtual...
-python -m venv venv
+echo Criando ambiente virtual, se necessario...
+if not exist venv\ (
+    python -m venv venv
+)
 
 echo Ativando ambiente virtual...
 call venv\Scripts\activate
 
-echo Instalando dependências...
+echo Instalando dependencias...
 pip install -r requirements.txt
 
-echo Rodando servidor Django...
-python manage.py runserver
-start "" http://127.0.0.1:8000/
+echo.
+echo --- INICIANDO O SERVIDOR DJANGO EM UMA NOVA JANELA ---
+start "" python manage.py runserver
+
+echo Abrindo o site no seu navegador...
+start http://127.0.0.1:8000/
+
+echo.
+echo O servidor Django esta rodando na outra janela.
+echo Pressione Ctrl+C naquela janela para parar o servidor.
+echo.
 pause
